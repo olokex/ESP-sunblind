cc91c6c807d910c1efe59bcab7197249aea05115
olok <olokex@gmail.com>
2024-04-20 22:06:50 +0200

    Update main.cpp
    new storing and retrieving running configuration
        now is used SPIFFS library instead of EEPROM
    small tweaks for the default configuration
    new end points
        fully closing to left/right
        fully open

--- main.cpp
+++ main.cpp
@@ -3,15 +3,16 @@
 #include <WebServer.h>
 #include <ArduinoJson.h>
 #include <Stepper.h>
-#include <EEPROM.h>
+#include <ArduinoOTA.h>
+#include <esp_system.h>
+#include <SPIFFS.h>
 
-constexpr int EEPROM_SIZE = 64;
-constexpr int EEPROM_MEMORY_ADDRESS = 0;
+static const char* CONFIG_FILE = "/config.txt";
 
 struct Configuration_t {
   int32_t stepsPerRevolution = 2048;
-  int32_t steppingSpeed = 3;
-  int32_t step = 1;
+  int32_t steppingSpeed = 20;
+  int32_t step = 3;
   int32_t maxOpen = 1000;
   int32_t maxClose = -1000;
   int32_t stepsCounter = 0;
@@ -29,7 +30,6 @@ const char* HOSTNAME = "ESP Test";
 constexpr int JSON_BUFFER_SIZE = 500;
 constexpr int PORT = 43332;
 
-
 int stepping = 0;
 bool handlingRequest = false;
 
@@ -45,6 +45,29 @@ Stepper myStepper(configuration.stepsPerRevolution, IN1, IN3, IN2, IN4);
 
 WebServer server(PORT);
 
+void saveConfiguration(const char *filename, const Configuration_t &config) {
+  File file = SPIFFS.open(filename, FILE_WRITE);
+  if (!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+
+  file.write((uint8_t *)&config, sizeof(Configuration_t));
+  file.close();
+}
+
+void loadConfiguration(const char *filename, Configuration_t &config) {
+  File file = SPIFFS.open(filename, FILE_READ);
+  if (!file) {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  file.read((uint8_t *)&config, sizeof(Configuration_t));
+  file.close();
+}
+
+
 void reset() {
   stepping = 0;
   handlingRequest = false;
@@ -54,8 +77,7 @@ void reset() {
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, LOW);
   
-  EEPROM.put(EEPROM_MEMORY_ADDRESS, configuration);
-  EEPROM.commit();
+  saveConfiguration(CONFIG_FILE, configuration);
 }
 
 void handlePost(int direction) {
@@ -93,8 +115,7 @@ void setConfiguration() {
   configuration.step = jsonDocument["step"];
   configuration.stepsCounter = jsonDocument["stepsCounter"];
 
-  EEPROM.put(EEPROM_MEMORY_ADDRESS, configuration);
-  EEPROM.commit();
+  saveConfiguration(CONFIG_FILE, configuration);
 
   Stepper newStepper(configuration.stepsPerRevolution, IN1, IN3, IN2, IN4);
   myStepper = newStepper;
@@ -122,20 +143,62 @@ void restoreDefaultConfig() {
   Configuration_t config;
   configuration = config;
 
-  EEPROM.put(EEPROM_MEMORY_ADDRESS, configuration);
-  EEPROM.commit();
-
+  saveConfiguration(CONFIG_FILE, configuration);
   getInfo();
 }
 
-void setup_routing() { 
+void handleFullyOpen() {
+  StaticJsonDocument<JSON_BUFFER_SIZE> jsonDocument;
+  String responseMessage;
+  int precalculatedSteps = 0;
+
+  if (configuration.stepsCounter >= 0) {
+    precalculatedSteps = -configuration.stepsCounter;
+    jsonDocument["message_positive"] = "Moving in positive: " + String(precalculatedSteps);
+  }
+
+  if (configuration.stepsCounter <= 0) {
+    precalculatedSteps = abs(configuration.stepsCounter);
+    jsonDocument["message_negative"] = "Moving in negative: " + String(precalculatedSteps);
+  }
+
+  jsonDocument["step_counter"] = configuration.stepsCounter;
+  serializeJson(jsonDocument, responseMessage);
+
+  handlingRequest = true;
+  myStepper.step(precalculatedSteps);
+  configuration.stepsCounter = 0;
+
+  reset();
+  server.send(HTTP_OK, "application/json", responseMessage);
+}
+
+void handleFullyClose(int direction) {
+  stepping = direction;
+  handlingRequest = true;
+
+  server.send(HTTP_OK, "application/json", "{}");
+}
+
+bool isInRange(int value, int boundary, int delta = 0) {
+  // Calculate the lower and upper bounds
+  int lowerBound = boundary - delta;
+  int upperBound = boundary + delta;
+
+  // Check if the value is within the bounds
+  return value >= lowerBound && value <= upperBound;
+}
+
+void setupRouting() { 
   server.on("/left", HTTP_POST, [](){handlePost(configuration.step);});    
-  server.on("/right", HTTP_POST, [](){handlePost(-configuration.step);});  
+  server.on("/right", HTTP_POST, [](){handlePost(-configuration.step);});
+  server.on("/open", HTTP_POST, handleFullyOpen);
+  server.on("/close/left", HTTP_POST, [](){handleFullyClose(configuration.step);});
+  server.on("/close/right", HTTP_POST, [](){handleFullyClose(-configuration.step);});
   server.on("/", HTTP_OPTIONS, [](){ server.send(HTTP_OK, "application/json", "{}");} );
   server.on("/configure", HTTP_POST, setConfiguration);
   server.on("/info", HTTP_GET, getInfo);
   server.on("/restoreDefaultConfig", HTTP_GET, restoreDefaultConfig);
-
   server.begin();    
 }
 
@@ -148,9 +211,6 @@ void setup() {
   WiFi.setHostname(HOSTNAME);
   WiFi.begin(ssid, password);
 
-  EEPROM.begin(EEPROM_SIZE);
-  EEPROM.get(EEPROM_MEMORY_ADDRESS, configuration);
-
   pinMode(LEFT_BUTTON_IN, INPUT);
   pinMode(RIGHT_BUTTON_IN, INPUT);
 
@@ -159,16 +219,25 @@ void setup() {
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
 
-  myStepper.setSpeed(configuration.steppingSpeed);
-
-  setup_routing();
+  setupRouting();
   Serial.begin(9600);
+  ArduinoOTA.begin();
+
+  if (!SPIFFS.begin(true)) {
+    Serial.println("An error has occurred while mounting SPIFFS");
+    return;
+  }
+
+  loadConfiguration(CONFIG_FILE, configuration);
+  myStepper.setSpeed(configuration.steppingSpeed);
 }
 
 //====================================================
 //         Loop
 //====================================================
 void loop() {
+  ArduinoOTA.handle();
+
   if (digitalRead(LEFT_BUTTON_IN)) {
     stepping = -configuration.step;
   } else if (digitalRead(RIGHT_BUTTON_IN)) {
@@ -187,6 +256,19 @@ void loop() {
   if (stepping < 0 && configuration.stepsCounter - stepping >= configuration.maxClose ) {
     myStepper.step(stepping);
     configuration.stepsCounter += stepping;
+  }
+
+  if (handlingRequest) {
+    if (stepping > 0) {
+      if (isInRange(configuration.stepsCounter, configuration.maxOpen, configuration.step)) {
+        reset();
+      }
+    }
+    if (stepping < 0) {
+      if (isInRange(configuration.stepsCounter, configuration.maxClose, configuration.step)) {
+        reset();
+      }
+    }
   }
 
   server.handleClient();
